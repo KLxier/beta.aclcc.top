f481cb3f082a7128d51ab262a6a06fd106eb8e44
myworldzycpc <myworldzhangyichen@qq.com>
2025-12-08 20:51:17 +0800

    迁移archive.aclcc.top成员数据

--- data/members/js/data.js
+++ data/members/js/data.js
@@ -1,56 +1,57 @@
+// 迁移自archive.aclcc.top，非个人见解
 const data = [
     {
-        "id": "example1",
-        "name": "成员1",
-        "detail": "成员1的简介",
+        "id": "grafrustix",
+        "name": "Grafrustix",
+        "detail": "神人 #1",
         "description": `
-            <p>成员1的简介</p>
-            <a href="https://space.bilibili.com/">B站</a>
+            <p>晴小姐魔改谱师兼ACLCC创始者</p>
+            <p><a class="bilibili" href="https://space.bilibili.com/3493270749448480">B站</a></p>
         `,
     },
     {
-        "id": "example2",
-        "name": "成员2",
-        "detail": "成员2的简介",
+        "id": "thehale-klxier",
+        "name": "TheHale (KLxier)",
+        "detail": "<del>尊贵的有钱人</del> (并非)",
         "description": `
-            <p>成员2的简介</p>
-            <a href="https://space.bilibili.com/">B站</a>
+            <p>三角魔改谱师兼域名持有者</p>
+            <p><a class="bilibili" href="https://space.bilibili.com/1749280432">B站</a></p>
         `,
     },
     {
-        "id": "example3",
-        "name": "成员3",
-        "detail": "成员3的简介",
+        "id": "yangsy56302",
+        "name": "Yangsy56302",
+        "detail": "神人 #2",
         "description": `
-            <p>成员3的简介</p>
-            <a href="https://space.bilibili.com/">B站</a>
+            <p>文字魔改谱师</p>
+            <p><a class="bilibili" href="https://space.bilibili.com/430612354">B站</a></p>
         `,
     },
     {
-        "id": "example4",
-        "name": "成员4",
-        "detail": "成员4的简介",
+        "id": "baile",
+        "name": "摆了",
+        "detail": "开摆这一块",
         "description": `
-            <p>成员4的简介</p>
-            <a href="https://space.bilibili.com/">B站</a>
+            <p>萝卜魔改谱师兼网站初版设计师</p>
+            <p><a class="bilibili" href="https://space.bilibili.com/672178840">B站</a></p>
         `,
     },
     {
-        "id": "example5",
-        "name": "成员5",
-        "detail": "成员5的简介",
+        "id": "ms-win-and-mc",
+        "name": "ms_win_and_mc",
+        "detail": "复制这一块",
         "description": `
-            <p>成员5的简介</p>
-            <a href="https://space.bilibili.com/">B站</a>
+            <p>网站美化师</p>
+            <p><a class="bilibili" href="https://space.bilibili.com/496241761">B站</a></p>
         `,
     },
     {
-        "id": "example6",
-        "name": "成员6",
-        "detail": "成员6的简介",
+        "id": "redstonelol",
+        "name": "一块红石",
+        "detail": "@一块红石 都怪你😡",
         "description": `
-            <p>成员6的简介</p>
-            <a href="https://space.bilibili.com/">B站</a>
+            <p>Gra 他爹</p>
+            <p><a class="bilibili" href="https://space.bilibili.com/3546965581236695">B站</a></p>
         `,
     },
 ];
